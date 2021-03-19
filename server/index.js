3d591ff0fab7737a7b1420142b43e3802e1c3a06
Avilio Muñoz <aviliomuoz@gmail.com>
2021-03-19 17:24:02 -0500

    CORS added

--- server/index.js
+++ server/index.js
@@ -3,9 +3,11 @@ const api = require('./api');
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const app = express();
 
+app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static('public'));
